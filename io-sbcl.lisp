8e1bc1b54baf83b62ba6089aea70032dcd1614c0
Stas Boukarev <stassats@gmail.com>
2012-02-18 03:09:32 +0400

    Get rid of compiler notes.

--- io-sbcl.lisp
+++ io-sbcl.lisp
@@ -154,7 +154,7 @@
                                                 :supersede)
                                  :if-does-not-exist :create
                                  :element-type '(unsigned-byte 8))
-       
+       (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
        (let* ((,size-sym ,size)
               (,length-sym (and ,append
                                (file-length ,fd-stream)))
